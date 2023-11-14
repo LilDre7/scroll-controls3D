aae899fc87809ead83be9258cd2d1d567eeda88f
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-11-14 11:29:29 -0600

    :memo: Add or update documentation

--- src/components/utils.jsx
+++ src/components/utils.jsx
@@ -1,3 +1,23 @@
 import * as THREE from "three";
 
-export const GenerateInitMaterials = () => {};
+export const GenerateInitMaterials = () => {
+  const cristalMaterial = new THREE.MeshPhysicalMaterial({
+    color: 0x8c8c8c,
+    metalness: 0.0,
+    roughness: 0.2,
+    transparent: true,
+    opacity: 1,
+    envMapIntensity: 1.0,
+    transmission: 1,
+  });
+
+  const sodaMaterial = new THREE.MeshStandardMaterial({
+    color: 0x000,
+    roughness: 1,
+    opacity: 1,
+    metalness: 1,
+    transparent: false,
+  });
+
+  return [cristalMaterial, sodaMaterial];
+};
